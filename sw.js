834f907d04d436fdff82c37973c98bb2b8e97dde
Morgan Cope <313798765+Morgatron78@users.noreply.github.com>
2026-09-10 19:08:05 +0100

    Restore iPhone header clearance and use vector pictogram

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'railboard-shell-v20';
+const CACHE = 'railboard-shell-v21';
 const ASSETS = ['./', './index.html', './styles.css', './src/app.js', './src/api.js', './src/storage.js', './src/config.js', './src/provider.js', './src/led.js', './manifest.webmanifest', './icons/favicon.png', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 ASSETS.push('./icons/wordmark.svg');
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
